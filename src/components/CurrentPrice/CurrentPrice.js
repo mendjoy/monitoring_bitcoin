a4839f669da7d1b2f7b3756049c6891d2460249f
joyce <joycemendes.jm79@gmail.com>
2023-02-21 17:36:51 -0300

    ultima cotação do dolar

--- src/components/CurrentPrice/CurrentPrice.js
+++ src/components/CurrentPrice/CurrentPrice.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import styles from "./style"
 
 const CurrentPrice = () => {
+
+  const [currentPrice, setCurrentPrice] = useState()
+  const url = "https://economia.awesomeapi.com.br/json/last/usd"
+ 
+
+  const getCurrentPriceUsd = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
     
+     setCurrentPrice(data.USDBRL);
+  };
+
+  useEffect(() => {
+    const url = "https://economia.awesomeapi.com.br/json/last/usd" 
+    getCurrentPriceUsd(url); 
+
+  }, [setCurrentPrice])
+
+  
   return (
     <View style={styles.headerPrice}>
-        <Text style={styles.currentPrice}>42223.5566</Text>
-        <Text style={styles.textPrice}>Ultima cotação</Text>
+        <Text style={styles.currentPrice}>
+          <Text style={styles.coin}>R$ </Text>
+          {currentPrice === undefined  && <Text>Carregando...</Text>}
+          {currentPrice != undefined  && <Text>{currentPrice.high}</Text>}
+          
+        </Text>
+        <Text style={styles.textPrice}>Última cotação do Dólar</Text>
     </View>
   )
 }
--- src/components/CurrentPrice/style.js
+++ src/components/CurrentPrice/style.js
@@ -5,12 +5,16 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "auto",
         alignItems: "center",
+        marginBottom: 20
     },
     currentPrice:{
         color: "#008000",
         fontSize: 32,
         fontWeight: "bold",
         paddingTop: 20
+    },
+    coin: {
+        color: "#fff"
     },
     textPrice:{
         color: "#fff",
